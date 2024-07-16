47b97ff9cf3fb711f5ca79cf431fd899eaf4095d
LevkoBe <lbeniakh@kse.org.ua>
2024-07-16 21:24:39 +0300

    playing state tests

--- src/manager_states/playing_state.rs
+++ src/manager_states/playing_state.rs
@@ -48,7 +48,11 @@ impl ManagerStateBehavior for PlayingState {
                 game_flow.hotel.announce();
             }
             "clear" => {
-                let address = game_flow.current_moving_player;
+                let player_number = game_flow.current_moving_player;
+                let address = game_flow.residents[player_number]
+                    .lock()
+                    .unwrap()
+                    .apartment_number;
                 game_flow.hotel.apartments[address].clear_mails();
                 println!("Your mails were cleared.");
             }
@@ -74,11 +78,11 @@ impl ManagerStateBehavior for PlayingState {
             }
             "restart" => {
                 println!("Game restarted");
-                return HandlingResult::ResetState;
+                return HandlingResult::Restart;
             }
             "new" if input.len() > 1 && input[1] == "game" => {
                 println!("New game started");
-                return HandlingResult::Restart;
+                return HandlingResult::ResetState;
             }
             "help" => {
                 println!("Available commands:");
@@ -86,10 +90,122 @@ impl ManagerStateBehavior for PlayingState {
                 println!("cheat -- allows to do something unallowed");
                 println!("pause -- changes state back to 'game'");
                 println!("restart -- restarts the current game");
-                println!("new game -- starts a new game");
+                println!("new -- starts a new game");
             }
             _ => println!("Invalid command"),
         }
         HandlingResult::KeepState
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{manager::Manager, manager_states::manager_state::ManagerState};
+
+    use super::*;
+
+    fn run_commands(manager: &mut Manager, commands: &[&str]) {
+        for command in commands {
+            let input: Vec<&str> = command.trim().split_whitespace().collect();
+            if !input.is_empty() {
+                manager.handle_command(&input);
+            }
+        }
+    }
+
+    #[test]
+    fn test_move_command() {
+        let mut manager = Manager::new();
+        let commands = vec!["hotel set", "residents settled", "play", "move"];
+
+        run_commands(&mut manager, &commands);
+        // This is a print test and will require manual checking of the output.
+    }
+
+    #[test]
+    fn test_hotel_command() {
+        let mut manager = Manager::new_with_state(ManagerState::Playing(Box::new(PlayingState)));
+        let commands = vec!["hotel default"];
+
+        run_commands(&mut manager, &commands);
+        // This is a print test and will require manual checking of the output.
+    }
+
+    #[test]
+    fn test_clear_command() {
+        let mut manager = Manager::new();
+        let commands = vec!["hotel set", "residents settled", "play", "clear"];
+
+        run_commands(&mut manager, &commands);
+
+        let player_number = manager.game_flow.current_moving_player;
+        let address = manager.game_flow.residents[player_number]
+            .lock()
+            .unwrap()
+            .apartment_number;
+
+        assert_eq!(manager.game_flow.hotel.apartments[address].mails.len(), 0);
+    }
+
+    #[test]
+    fn test_cheat_command() {
+        let mut manager = Manager::new_with_state(ManagerState::Playing(Box::new(PlayingState)));
+        let commands = vec!["cheat"];
+
+        run_commands(&mut manager, &commands);
+        // This is a print test and will require manual checking of the output.
+    }
+
+    #[test]
+    fn test_pause_command() {
+        let mut manager = Manager::new_with_state(ManagerState::Playing(Box::new(PlayingState)));
+        let commands = vec!["pause", "move"];
+
+        run_commands(&mut manager, &commands);
+    }
+
+    #[test]
+    fn test_reveal_command() {
+        let mut manager = Manager::new_with_state(ManagerState::Playing(Box::new(PlayingState)));
+        let commands = vec!["reveal"];
+
+        run_commands(&mut manager, &commands);
+        // This is a print test and will require manual checking of the output.
+    }
+
+    #[test]
+    fn test_whoami_command() {
+        let mut manager = Manager::new();
+        let commands = vec!["hotel set", "residents settled", "play", "whoami"];
+
+        run_commands(&mut manager, &commands);
+        // This is a print test and will require manual checking of the output.
+    }
+
+    #[test]
+    fn test_restart_command() {
+        let mut manager = Manager::new_with_state(ManagerState::Playing(Box::new(PlayingState)));
+        let commands = vec!["restart"];
+
+        run_commands(&mut manager, &commands);
+        // The state change will need to be verified manually or with additional mock logic.
+    }
+
+    #[test]
+    fn test_new_game_command() {
+        let mut manager = Manager::new_with_state(ManagerState::Playing(Box::new(PlayingState)));
+        let commands = vec!["new"];
+
+        run_commands(&mut manager, &commands);
+        // The state change will need to be verified manually or with additional mock logic.
+    }
+
+    #[test]
+    fn test_help_command() {
+        let mut manager = Manager::new_with_state(ManagerState::Playing(Box::new(PlayingState)));
+        let commands = vec!["help"];
+
+        run_commands(&mut manager, &commands);
+        // This is a print test and will require manual checking of the output.
     }
 }
